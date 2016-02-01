28d7fe059431d75afa240fb2f4d75a3f4d10deab
RyanLPeterman <RyanLPeterman@gmail.com>
2016-01-31 17:15:59 -0800

    Added Google Sheets, Stat Tracking, and Exception Handling

--- get_occupants.py
+++ get_occupants.py
@@ -2,13 +2,17 @@
 #
 # Lab Occupancy Sensor - to tell if officers are in the lab
 # 		for the general members
-# TODO: 1. Write out the data to google sheets when the script fails
-#		2. Poll less often
-#		3. Add Status setting abilty to bot
-#		4. treat officers differently based on ability
-#		5. treat errors so the program never crashes
+# TODO: 1. Write out the data to google sheets when the script fails (DONE)
+#		2. Poll less often (DONE)
+#		3. Add Status setting abilty to bot (DONE)
+#		4. treat officers differently based on status (DONE)
+#		5. treat errors so the program never crashes 
+#		6. Add lab general statistics (busiest time of day)
+# 		7. Add easter eggs
+#		8. Add weekly statistics
 
 import subprocess
+import os
 import json
 import time
 from slackclient import SlackClient
@@ -16,7 +20,7 @@ import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
 # Token from slack for bot API
-bot_token = 0
+bot_token = ""
 
 # list of officers
 officer_list = []
@@ -24,88 +28,149 @@ officer_list = []
 class Officer:
 	name = ""
 	mac_addr = ""
-	device = ""
-	status = ""
-	hours = 0
+	status = 0 # 0 == online, 1 == tracked 
 	minutes = 0
-	seconds = 0
-
+	is_in_lab = False
+	miss_count = 0 # if this gets to 5 we remove them from the list
+				   # if they are seen on the scan we set it to 0
 	# default constructor
 	def __init__(self):
 		self.name = ""
 		self.mac_addr = ""
-		self.device = ""
-		self.status = ""
-		self.hours = 0
+		self.status = 0
 		self.minutes = 0
-		self.seconds = 0
+		self.is_in_lab = False
+		self.miss_count = 0
 
 	# print officer function
 	def print_officer(self):
 		print "-------------------------"
 		for m_data in [a for a in dir(self) if not a.startswith('__') and not callable(getattr(self,a))]:
-			print m_data + " = " + getattr(self, m_data)
+			print m_data + " = " + str(getattr(self, m_data))
 
-def get_officers():
+def run_scan():
 	""" Returns a string of all the recognized people according to the list
 	of MAC addresses in mac_addresses.txt in the cwd
-	This function also populates seconds dict everytime it is ran
+	This function also populates officer_list times every time
 	NOTE: YOU MUST RUN THIS AS ROOT AND HAVE ETHERNET CONENCTION"""
 
-	# store arp output
-	try:
-		arp_output = subprocess.check_output(["arp-scan", "-l"])
-	except Exception:
-		return "arp-scan Failed! Please try again!\n"
+	# store arp output (max 20 retries)
+	for attempt in xrange(20):
+		try:
+			arp_output = subprocess.check_output(["arp-scan", "-l"])
+		except Exception:
+			# skip break and try again if error when running arp-scan
+			continue
+
+		# break if arp-scan worked
+		break
+
+	# didnt work even after 20 tries
+	if not arp_output:
+		print "Error: arp-scan did not work correctly even after 20 tries"
+
+	# used to add to miss count
+	scan_hit = False
+
+	# find if any officer mac address is found in the arp_output
+	for officer in officer_list:
+		for line in arp_output.splitlines():
+			if officer.mac_addr in line:
+				# we know they are in the lab
+				officer.is_in_lab = True
+				officer.miss_count = 0
+				scan_hit = True
+
+				print "Found : " + officer.name
+
+				# use this for timing statistics
+				officer.minutes += 1
+	
+		# if this officer not found in arp-scan				
+		if not scan_hit:
+			officer.miss_count += 1
+		# officer was found and set scan_hit back to false for next officer
+		else:
+			scan_hit = False
+
+		# they have been missing for more than 5 minutes
+		if officer.miss_count > 5:
+			officer.is_in_lab = False
+	
+
+def exit_handler():
+	# init google sheets api
+	json_key = json.load(open('occupancy sensor-52452f4f1313.json'))
+	scope = ['https://spreadsheets.google.com/feeds']
+	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+	gc = gspread.authorize(credentials)
+	wks = gc.open("Lab Occupancy Sensor").sheet1
+
+	# write-out all the data to google sheets
+	for officer in officer_list:
+
+		# holds cell of officer name
+		cell = wks.find(officer.name)
+
+		# write out the officers information TODO: don't hardcode this
+		wks.update_cell(cell.row, 4, officer.status)
+		wks.update_cell(cell.row, 5, officer.minutes)
+
+	print "Wrote out to google sheets!"
+		
+def get_officers():
 
 	# build up newline delimited string of officers
 	officer_str = ""
 
-	# find if any officer mac address is found in the arp_output
-	for line in arp_output.splitlines():
-		for officer in officer_list:
-			if officer.mac_addr in line:
-				officer_str += officer.name + "\n"
+	# if officer is in lab add to str
+	for officer in officer_list:
+		if officer.is_in_lab and not officer.status:
+			officer_str += officer.name + "\n"
 
 	if not officer_str:
 		return "To my knowledge it appears no one is in the lab. Please try again to be sure!"
 	
 	return officer_str
-	
 
-def test_chat():
-	# initialize handle
-	sc = SlackClient(bot_token)
+def get_top_officers():
 
-	# get json object corresponding to opening chat with Ryan Peterman
-	im_dict = json.loads(sc.api_call("im.open", user="U0F8HE81Y"))
-	chan_id = str(im_dict["channel"]["id"])
-	greet_str = "Hello!\n Nice to meet you!\n"
+	top_list = sorted(officer_list, key=lambda x: x.minutes , reverse=True)
 
-	# call chat.postMessage to send message to the person
-	print sc.api_call("chat.postMessage", as_user="true:", channel=chan_id, text=greet_str)
+	top_str = ""
+
+	for index in xrange(1, 4):
+		cur_officer =  top_list[index - 1]
+		hours = cur_officer.minutes / 60
+		minutes = cur_officer.minutes % 60
+
+		if cur_officer.status:
+			name = "John Cena (Anonymous)"
+		else:
+			name = cur_officer.name
+
+		top_str += str(index) + ". " + name + " with " + str(hours) +" hours and " + str(minutes) + " minutes."+ "\n"
+
+	return top_str
+
 
 def init_officers():
 	""" Populates all the officer objects with their data
 	from the google sheets API"""
 	json_key = json.load(open('occupancy sensor-52452f4f1313.json'))
 	scope = ['https://spreadsheets.google.com/feeds']
-	# print hello
-
 	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-
 	gc = gspread.authorize(credentials)
-
+	# open worksheet
 	wks = gc.open("Lab Occupancy Sensor").sheet1
-
-	# create list of officer objects
 	num_officers = wks.row_count - 1
 
+	# create list of officer objects
 	for i in xrange(num_officers):
 		officer_list.append(Officer())
 
 	# populate officer objects with data from spreadsheet
-	for col_label in ["Name", "Mac Address", "Device", "Status", "Hours", "Minutes", "Seconds"]:
+	for col_label in ["Name", "Mac Address", "Status", "Minutes"]:
 		
 		cell = wks.find(col_label)
 		values = wks.col_values(cell.col)
@@ -113,70 +178,115 @@ def init_officers():
 		for value, officer in zip(values[1:], officer_list):
 			
 			if col_label == "Name":
-				officer.name = value
+				officer.name = str(value)
 			elif col_label == "Mac Address":
 				# lower because this is how arp-scan outputs it
-				officer.mac_addr = value.lower()
-			elif col_label == "Device":
-				officer.device = value
+				officer.mac_addr = str(value.lower())
 			elif col_label == "Status":
-				officer.status = value
-			elif col_label == "Hours":
-				officer.hours = value
+				officer.status = int(value)
 			elif col_label == "Minutes":
-				officer.minutes = value
-			elif col_label == "Seconds":
-				officer.seconds = value
-	# To print officers
-	for i in officer_list:
-		i.print_officer()
+				officer.minutes = int(value)
+
+	# # To print officers
+	# for i in officer_list:
+	# 	i.print_officer()
 
 def main():
 	# init bot token and officers from google sheets
 	sc = SlackClient(bot_token)
-	init_officers()
+
+	for attempt in xrange(20):
+		try:
+			init_officers()
+		except Exception:
+			if attempt == 19:
+				# exit without running finally block
+				# since we were not able to load from gdocs.
+				# we do not want to overwrite with null values during cleanup
+				os._exit(2)
+			continue
+		break
+
 
 	# TODO: Dynamically find the bot's id using users.list
 	# could cause problem if this changes for some reason
 	bot_id = "U0H7GEEJW"
 
+	# counts up after every sleep(1)
+	# so we can poll when counter reachs 60 or 1 min
+	counter = 0
+
+	# conenct to the bots feed
 	if sc.rtm_connect():
 		while True:
+			# read events from peterbot's feed
 			events = sc.rtm_read()
 			print events
+			print counter
 
 			for e in events:
 				message = ""
 				user_input = ""
 
+				# format the input text
 				if e.get("text"):
-					user_input = e.get("text").lower()
+					user_input = e.get("text").lower().strip()
 
 				# if bot received text "whois"
 				if user_input == "whois":
 					# reply with list of officers
 					message = get_officers()
+
+				elif user_input == "status":
+					try:
+						user_dict = json.loads(sc.api_call("users.info", user=e.get("user")))
+					except Exception: 
+						message = "Failed to load users when looking up your name"
 					
-				elif user_input == "time":
-					user_dict = json.loads(sc.api_call("users.info", user=e.get("user")))
-					
-					# grabs real name of user in homes of it corresponding
-					# to the name in the mac_addresses.txt file
+					# grabs real name of user
 					name = user_dict["user"]["profile"]["real_name"]
 
-					# TODO fix this to be a better implementation of timing
-					# if seconds.get(name):						
-					# 	# reply with seconds of the corresponding user 
-					# 	message = "In the last week, you have been in the lab for: " + str(seconds.get(name)) + " seconds."
-					# else:
-					# 	message = "Either your slack real_name doesn't match with my archives or you haven't been in the lab"
-						
+					for officer in officer_list:
+
+						if officer.name == name:
+							officer.status = 1 - officer.status
+
+							if not officer.status:
+								message = "You are currently tracked/online."
+								officer.is_in_lab = True
+							else:
+								message = "You are currently not tracked/offline. You are not visible to others but are still gaining time in the lab statistics. \n"
+								officer.is_in_lab = False
+				
+				elif user_input == "top":
+					message = get_top_officers()
+
+				elif user_input == "time":
+					try:
+						user_dict = json.loads(sc.api_call("users.info", user=e.get("user")))
+					except Exception: 
+						message = "Failed to load users when looking up your name"
+					
+					# grabs real name of user
+					name = user_dict["user"]["profile"]["real_name"]
+
+					for officer in officer_list:
+	
+						if officer.name == name:
+							minutes = officer.minutes % 60
+							hours = officer.minutes / 60
+
+							message = "You currently have a total of " + str(hours) + " hours, " + str(minutes) + " minutes in the lab." 
+
+					if not message:
+						message = "You are not in the google sheet."
 
 				elif e.get("text"):
 					message = "Here are the following commands I support:\n \
 					whois - prints people currently in the lab \n \
 					time - prints how long you were in the lab this past week \n \
-					help - prints out all options \n"
+					status - toggle your status to online/offline \n \
+					top - prints the top three time totals \n"
 
 				# if there is a message to send, then send it
 				# will not respond if received from bot message to prevent
@@ -185,13 +295,21 @@ def main():
 					chan_id = e.get("channel")
 					sc.api_call("chat.postMessage", as_user="true:", channel=chan_id, text=message)
 
-			# TODO run arp-scan quietly every minute or so quietly for statistics
-
 			# delay
 			time.sleep(1)
+			counter += 1
+
+			# run script to build up statistics
+			if counter >= 60:
+				counter = 0
+				# run quietly
+				run_scan()
 	else:
 		print "Connection Failed: invalid token"
 
 # runs main if run from the command line
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	finally:
+		exit_handler()
